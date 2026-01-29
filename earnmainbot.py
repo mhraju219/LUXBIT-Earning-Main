ab5ba9d5c75b77685fee377df0083fd03a602fdf
mhraju219 <mhraju219@gmail.com>
2026-01-29 06:48:20 +0300

    Update earnmainbot.py

--- earnmainbot.py
+++ earnmainbot.py
@@ -1,11 +1,9 @@
 import os
 import logging
-
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 logging.basicConfig(level=logging.INFO)
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -17,7 +15,6 @@ def main():
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    # Use polling first for testing
     app.run_polling()
 
 if __name__ == "__main__":
